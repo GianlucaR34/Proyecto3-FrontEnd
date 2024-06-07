678b45d4bb9f63bb9c666a95336af4aabb07b2a9
Gianluca Sanguinetti <giansanguinetti77@gmail.com>
2024-06-07 02:33:17 -0300

    responsive realizado se ve correctamente en todos los dispositivos

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api'; // Importa componentes de Google Maps
+import React, { useRef, useEffect } from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 import '../css/MapContainer.css';
+
 function MapContainer() {
+	const mapRef = useRef(null);
+	const markerRef = useRef(null);
+
 	// Coordenadas del lugar
-	const center = { lat: -34.603722, lng: -58.381592 };
+	const center = { lat: -26.8233, lng: -65.2775 };
 
 	// Opciones del mapa
 	const mapOptions = {
@@ -11,16 +15,29 @@ function MapContainer() {
 		center: center,
 	};
 
+	useEffect(() => {
+		if (window.google && window.google.maps && window.google.maps.marker) {
+			const { AdvancedMarkerElement } = window.google.maps.marker;
+			if (mapRef.current && !markerRef.current) {
+				markerRef.current = new AdvancedMarkerElement({
+					position: center,
+					map: mapRef.current.state.map,
+				});
+			}
+		}
+	}, []);
+
 	return (
-		<LoadScript googleMapsApiKey="AIzaSyBdqmPPjySntUN6MMWwMx_FymVvJ5VTAF8">
-			{' '}
-			{/* Reemplaza TU_API_KEY con tu clave de API de Google Maps */}
+		<LoadScript
+			googleMapsApiKey="AIzaSyBdqmPPjySntUN6MMWwMx_FymVvJ5VTAF8"
+			onLoad={() => console.log('Google Maps API loaded successfully')}
+			onError={(e) => console.error('Error loading Google Maps API', e)}
+		>
 			<GoogleMap
 				mapContainerStyle={{ height: '100%', width: '100%' }}
 				options={mapOptions}
-			>
-				{/* Contenido del mapa, como marcadores o capas */}
-			</GoogleMap>
+				onLoad={(map) => (mapRef.current = map)}
+			/>
 		</LoadScript>
 	);
 }
